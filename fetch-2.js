3e7be56fe2f98b1eec647c8c0a5146f74a4a0062
Matias Nicolas Michelli <proga2332@gmail.com>
2021-06-14 23:13:22 -0300

    Peticion Fetch API

---
+++ fetch-2.js
@@ -0,0 +1,12 @@
+
+// Peticion GET
+// https://reqres.in/api/users
+
+fetch( 'https://reqres.in/api/users' )
+    .then(resp => resp.json())
+    .then(respObj => {
+        console.log(respObj);  //{page: 1, per_page: 6, total: 12, total_pages: 2, data: Array(6), …}
+        console.log(respObj.page); // 1
+        console.log(respObj.data); //(6) [{…}, {…}, {…}, {…}, {…}, {…}]
+        console.log(respObj.support); //{url: "https://reqres.in/#support-heading", text: "To keep ReqRes free, contributions towards server costs are appreciated!"}
+    })
