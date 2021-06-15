ec70e4a40d31be9256f63f7d12175bed1b338cee
Matias Nicolas Michelli <proga2332@gmail.com>
2021-06-15 00:59:10 -0300

    Actividad

---
+++ fetch-6.js
@@ -0,0 +1,19 @@
+
+
+
+fetch('noFound.html')
+    .then( resp => resp.text() )
+    .then( html => {
+        let body = document.querySelector('body');
+        body.innerHTML = html;
+
+        // console.log(html);
+    })
+    .catch( error => {
+        console.log('Error en la peticion');
+        console.log(error);
+    });
+
+
+
+
